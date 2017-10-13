077dcefbb543f8b36a472d78b8ec06e67d0d0805
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-10-13 13:30:58 -0300

    changed function

--- branch_n_bound.m
+++ branch_n_bound.m
@@ -13,6 +13,9 @@ A = input("Enter the coefficients of the right hand side of the\
 B = input("Enter the left hand side of the constraints in the form of\
  a column vector: ")
 
+assert (rows(f) == columns(A));
+assert (rows(A) == rows(B));
+
 % Number of variables = number of coefficients in f
 num_variables = numel(f);
 
@@ -21,37 +24,41 @@ lb = zeros(num_variables, 1);
 ub = inf*ones(num_variables, 1);
 
 function retval = bb(f, A, B, Aeq, Beq, lb, ub)
+
+    global num_variables;
+
     [v, X] = linprog(f, A, B, Aeq, Beq, lb, ub);
     flag_int = 1;
 
     for i =  0:num_variables
-        if mod(X(i),1)
+        if mod(X(i,0),1)
             flag_int = 0;
-            var_val = floor(X(i));
+            var_val = floor(X(i,0));
             var_index = i;
         endif;
     endfor;
 
     if flag_int
         retval = [v, X];
+    else
+        lb1 = lb;
+        lb2 = lb;
+        ub1 = ub;
+        ub2 = ub;
+
+        lb1(var_index,0) = var_val+1;
+
+        ub2(var_index,0) = var_val;
+
+        [v1, X1] = bb(f, A, B, Aeq, Beq, lb1, ub1);
+        [v2, X2] = bb(f, A, B, Aeq, Beq, lb2, ub2);
+
+        if v1 < v2
+            retval = [v1, X1];
+        endif;
+
+        retval = [v2, X2];
     endif;
-
-    lb1 = lb;
-    lb2 = lb;
-    ub1 = ub;
-    ub2 = ub;
-
-    lb1[var_index] = var_val+1;
-
-    ub2[var_index] = var_val;
-
-    [v1, X1] = bb(f, A, B, Aeq, Beq, lb1, ub1);
-    [v2, X2] = bb(f, A, B, Aeq, Beq, lb2, ub2);
-
-    if v1 < v2
-        retval = [v1, X1];
-
-    retval = [v2, X2];
 endfunction;
 
 [v, X] = bb(f, A, B, Aeq, Beq, lb, ub)
